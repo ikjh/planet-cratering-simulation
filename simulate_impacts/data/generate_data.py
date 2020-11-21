746aca7bd9fb0c0fa0472b98c02baa8dec481d37
ikjh <heinzekyle@gmail.com>
2020-11-21 00:41:12 -0700

    shifting

--- simulate_impacts/data/generate_data.py
+++ simulate_impacts/data/generate_data.py
@@ -1,9 +1,11 @@
-import powerlaw.powerlaw as pl
 import os
+from ..powerlaw import powerlaw
 
 dir = os.path.dirname(os.path.realpath(__file__))
 
+
 print(dir)
+'''
 #create power law distribution using minimum crater diameter of 10, using integers (discrete)
 distribution = pl.Power_Law(xmin=10, xmax=500, parameters=[2.5], discrete=True)
 
@@ -11,6 +13,7 @@ distribution = pl.Power_Law(xmin=10, xmax=500, parameters=[2.5], discrete=True)
 
 data = distribution.generate_random(10000, estimate_discrete=True)
 
-datafile = open(dir + "\data.txt", 'w')
+datafile = open(dir + "\data\data.txt", 'w')
 for datum in data:
     datafile.write("%d\n" % int(datum))
+    '''
